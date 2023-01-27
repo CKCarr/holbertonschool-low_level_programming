8159dcd4cbbb29cd8b771e6503104076afc72e99
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-01-26 22:17:25 -0800

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - entry
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	for (int x = 0; x < 10; x++)
+	putchar('0' + x);
+	putchar ('\n');
+
+	return (0);
+}
