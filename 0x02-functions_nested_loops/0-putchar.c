2c5f07919ba351edf15463fe9b35fe287b6f3e84
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-01-28 08:48:11 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Print _putchar using only putchar.
+*
+* Return 0 Always 0 is void 0 is Success
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
