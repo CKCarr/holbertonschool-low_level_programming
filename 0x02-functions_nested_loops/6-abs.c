b0508e1fc7400c047ebad892eb4df95360b7246f
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-01-28 19:53:29 -0800

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
-* int _abs(int) - prototype to return absolute value
+*  _abs - prototype to return absolute value
 *
-* @int: is the integer checked
+* @x: is the integer checked
 *
 * Return: is void, Always 0
 */
 int _abs(int x)
 {
 	x = _abs(x);
+	return (0);
 }
