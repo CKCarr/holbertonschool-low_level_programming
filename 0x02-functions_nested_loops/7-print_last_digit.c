b7fb2878f68aa9966972154c8cd7bd79dbf5415c
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-01-29 14:14:27 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
-* print_last_digit - entry point ,print last digit of a value
+* print_last_digit - print last digit of a value
 * @d: digit to be checked
+* @ld: lastdigit of d
 * Return: value of last digit
 */
 
 int print_last_digit(int d)
 {
+	int ld = d % 10;
 
-	if (d % 2 != 0)
+	if (ld < 0)
 	{
-		_putchar(d % 10);
+		ld = ld * -1;
 	}
-	else
-	{
-		_putchar(d % 10);
-	}
-	return (0);
+	_putchar(ld + '0');
+
+	return (ld);
 }
