3d0a30de5b4f6bfd088c3388778d2c3d7b3aa830
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-01-28 18:00:54 -0800

    task islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _islower - prototype
+*
+* Return: 0 if true 1 if false
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
