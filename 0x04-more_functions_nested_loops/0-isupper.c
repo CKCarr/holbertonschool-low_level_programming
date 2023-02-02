5f6097a199badb7f6168634d1e39783e130e2aeb
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-02-01 16:55:21 -0800

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,18 @@
 /**
 * _isupper -  a function that checks for uppercase character.
 *
-*@c: the 
+*@c: the char to be checked
 *
-*
-*//
+* Return: 1 for uppercase 0 for otherwise
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
