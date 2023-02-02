41012536000360a8e6a09f0502c1705e2fa3d698
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-02-01 21:17:34 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 /**
 * print_numbers - function that prints #'s 0-9 followed be newline
 *
-*
+*@i: number to be checked
 *
 *Return: 0 is success
 *
@@ -11,7 +11,7 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar('0' + i);
 	}
