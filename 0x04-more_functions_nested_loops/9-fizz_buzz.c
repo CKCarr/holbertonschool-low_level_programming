0b4cb86a54865d5cdb874624433c327f94f8f16f
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-02-03 12:16:21 -0800

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 99; i++)
 	{
-		if ((i % 3) == 0) && ((i % 5) == 0)
+		if ((i % 15) == 0)
 		{
 			printf("FizzBuzz ");
 		}
@@ -29,6 +29,7 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
+		printf("buzz");
 		putchar('\n');
 	return (0);
 }
