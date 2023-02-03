6642a886268955e98dbefd2938d28a9307dbdaf4
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-02-03 11:37:28 -0800

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - Fizz Buzz test
+*
+*
+*
+* Return: void
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 15) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+		putchar('\n');
+		return (0);
+}
