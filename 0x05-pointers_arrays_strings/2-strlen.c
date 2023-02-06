130f52b1ad6cb85e89a39640d52d573cf8fe3e9b
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-02-06 15:04:55 -0800

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,14 @@
 *
 * @s: the char for the length to be determined.
 *
-* Return: int aka '#' of chars in given string will be returned.
+* Return: int aka '#' of chars in given string
 *
 */
 int _strlen(char *s)
 {
 	int length;
 
-       	length = 0;
+	length = 0;
 	while (s[length] != '\0')
 	{
 		(length++);
