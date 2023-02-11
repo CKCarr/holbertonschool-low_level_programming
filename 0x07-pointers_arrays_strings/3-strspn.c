9b6c6488dcb93303232eb30d41469e60d75d2d4a
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-02-11 13:49:04 -0800

    strspn prototype

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,27 +1,27 @@
 #include "main.h"
 /**
+* _strspn - function that gets length of a substring
 *
-* -strspn - function 
-*
-*
-*
-*
+* @s: string to be searched.
+* @accept: string of set chars to allow.
+* @ nb = number of bytes
+* Return: num of bytes of s which == bytes of set.
 *
 */
 unsigned int _strspn(char *s, char *accept)
 {
 	int si, ai, nb;
 
-	for (si = 0; s[si] != '\0' && s[i] != ' '; si++)
+	for (si = 0; s[si] != '\0' && s[si] != ' '; si++)
+	{
+		for (ai = 0; accept[ai] != '\0'; ai++)
 		{
-			for (ai = 0; accept[ai]; ai++)
+			if (s[si] == accept[ai])
 			{
-				if (s[si] == accept[ai])
-				{
-					nb++;
-					break;
-				}
+				nb++;
+				break;
 			}
 		}
+	}
 	return (nb);
 }
