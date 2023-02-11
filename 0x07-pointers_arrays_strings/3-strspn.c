182693f2b268dc61f3daf0587a52dcc6f032fb38
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-02-11 13:32:54 -0800

    strchr prototype

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* _strspn - function that gets length of a substring
 *
-* @s: string to be searched.
-* @accept: string of set chars to allow.
-* @ nb : number of bytes.
-* Return: num of bytes of s which == bytes of set.
+* -strspn - function 
+*
+*
+*
+*
 *
 */
 unsigned int _strspn(char *s, char *accept)
