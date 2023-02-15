76d551fd53bbccb1faaa27d2d38b5c3cc3fc2222
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-02-15 12:30:16 -0800

    task factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,5 +1,16 @@
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* factorial - return factorial of a given number
+*
+*@n: number to be checked
+*
+* Return: Always 0. unless its a factorial and then 1
+*/
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return(1);
+	else
+		return(n * factorial(n - 1));
+}
