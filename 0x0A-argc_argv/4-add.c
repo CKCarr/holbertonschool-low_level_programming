99afe5c644adbd9a30d8807e2d3d93daf5f673c8
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-02-18 18:23:15 -0800

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
+* main - program that adds positive numbers.
 *
+* @argc: count of arguments
+* @argv: array of arguments
 *
-*
-*
-*
-*
-*
-*
-*
+* Return: return 1 if !digit or 0num passed 0 for success
 */
+int main(int argc, char *argv[])
+{
+	int index, digit, result = 0;
+
+	for (index = 1; index < argc; index++)
+	{
+		for (digit = 0; argv[index][digit]; digit++)
+		{
+			if (argv[index][digit] < '0' || argv[index][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result += atoi(argv[index]);
+	}
+	printf("%d\n", result);
+	
+	return (0);
+}
