8f5393acecb38f706e5f168b637f5e520dec88f5
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-02-19 20:29:44 -0800

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,12 +13,16 @@ char *_strdup(char *str)
 	char *dupstr;
 	int index, len = 0;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 	for (index = 0; str[index]; index++)
 	{
 		len++;
 	}
 	dupstr = malloc(sizeof(char) * (index + 1));
-	if (str == NULL || dupstr == NULL)
+	if (dupstr == NULL)
 	{
 		return (NULL);
 	}
