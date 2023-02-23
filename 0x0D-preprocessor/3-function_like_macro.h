196622aff50d3f1f996829a1bef8f9090a8993fd
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-02-22 20:01:42 -0800

    preprocessing preprocessors

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
-* FILE: 3-function_like_macro.h
-*
-* DESCRIPTION:function-like macro ABS(x) that computer
-* the absolute value of a number x
-*/
-
 #define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
 
