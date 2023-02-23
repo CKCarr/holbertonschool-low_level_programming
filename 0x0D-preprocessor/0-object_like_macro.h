243be657d79b3f2aa3219ed0e148dabb4a9f9b30
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-02-22 17:42:36 -0800

    Processing Preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,17 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+
+/**
+* header file to define a macro named SIZE 
+* to be equivalent to the value 1024
+*
+* In this way we can call the macro 
+*
+*/
+
+
+
+#define SIZE = 1024
+
+#endif
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,20 @@
+#ifndef PI_H
+#define PI_H
+
+
+/**
+*
+*
+*
+*
+*
+*
+*
+*/
+
+
+
+#define PI = 3.14159265359 
+
+
+#endif
