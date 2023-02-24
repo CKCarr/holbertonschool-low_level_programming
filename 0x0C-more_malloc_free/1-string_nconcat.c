9a68d03e8f7b5cbddebe14ca32dcfa6423931bfc
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-02-24 13:46:33 -0800

    concat 2 strings in memalloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,40 +6,35 @@
 *
 * @s1: char string one
 * @s2: char string two be added on to 1st.
-* @n: size of memory to be allocated.
+* @n: char bytes of s2.
 *
 * Return: pointer to new memory allocation
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ccstr;
-	unsigned int s1_i = 0, s2_i = 0;
 	unsigned int s1_l = 0, s2_l = 0;
+	unsigned int s1_i, s2_i;
 
 	if (s1 == NULL || s2 == NULL)
-	{
-		return ("");
-	}
+		return (NULL);
 
-	for (s1_l = 0; s1[s1_l] != '\0'; s1_l++)
-	{
-		for (s2_l = 0; s2[s2_l] != '\0'; s2_l++)
-		{
-			if (n >= s2_l)
-			{
-				s2_l++;
-			}
-		}
-	}
+	while (s1[s1_l] != '\0')
+		s1_l++;
+	while (s2[s2_l] != '\0' && s2_l < n)
+		s2_l++;
 
-	ccstr = malloc(sizeof(char) * (s1_l + s2_l + 1));
+	if (n >= s2_l)
+		n = s2_l;
+
+	ccstr = malloc(s1_l + n + 1);
 
 	if (ccstr == NULL)
 		return (NULL);
 
 	for (s1_i = 0; s1_i < s1_l; s1_i++)
 		ccstr[s1_i] = s1[s1_i];
-	for (s2_i = 0; s2_i < s2_l; s2_i++)
+	for (s2_i = 0; s2_i < n ; s2_i++)
 		ccstr[s1_i + s2_i] = s2[s2_i];
 
 	ccstr[s1_i + s2_i] = '\0';
