df50ccc35c77e9c6671d3c1ad4c349f361b22312
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-02-24 01:15:51 -0800

    lost in translation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,45 +9,35 @@
 * @n: size of memory to be allocated.
 *
 * Return: pointer to new memory allocation
-* upon fail retun NULL.
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ccstr;
-	unsigned int size = n, str_i;
-
+	unsigned int stri_1, stri_2, size;
+	/*handle null pointers*/
 	if (s1 == NULL)
-	{
 		return ("");
-	}
 	if (s2 == NULL)
-	{
 		return ("");
-	}
-	
-	for (str_i = 0; s1[str_i] || s2[str_i]; str_i++)
-	{
-		size++;
-	}
-	if (n >= s2[str_i])
-	{
-		n = s2[size];
-	}
-
-	ccstr = malloc(sizeof(char) * (size + n + 1));
+	/*get length of each string*/
+	for (stri_1 = 0; s1[stri_1]; stri_1++)
+		stri_1++;
+	for (stri_2 = 0; s2[stri_2]; stri_2++)
+		stri_2++;
+	/*if n is greater than stri_2 use whole string*/
+	if (n >= stri_2)
+		n = stri_2;
+	/*allocate memory for the concaternated string*/
+	ccstr = malloc(sizeof(char) * (stri_1 + n + 1));
 	if (ccstr == NULL)
-	{
 		return (NULL);
-	}
+	/*copy concat str into ccstr*/
 	size = 0;
-	for (str_i = 0; s1[str_i]; str_i++)
-	{
-		ccstr[size++] = s1[str_i];
-	}
-	for (str_i = 0; s2[str_i] && str_i < n; str_i++)
-	{
-		ccstr[size++] = s2[str_i];
-	}
-	ccstr[str_i + size] = '\0';
+	for (stri_1 = 0; s1[stri_1]; stri_1++)
+		ccstr[size++] = s1[stri_1];
+	for (stri_2 = 0; s2[stri_2] && stri_2 < n; stri_2++)
+		ccstr[size++] = s2[stri_2];
+	/*null terminate the string*/
+	ccstr[stri_1 + n] = '\0';
 	return (ccstr);
 }
