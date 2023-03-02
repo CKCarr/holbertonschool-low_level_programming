dab75cfed590800271d73eba87b16c6dee3e8215
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-03-02 14:26:18 -0800

    print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 #include <stdlib.h>
-#include <stddef.h>
 /**
   * print_name - function that prints a name
   *
@@ -11,5 +10,11 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	int index;
+
+	for (index = 0; name[index] != '\0'; index++)
+	{
+		(*f)(&name[index]);
+	}
+	(*f)("\n");
 }
