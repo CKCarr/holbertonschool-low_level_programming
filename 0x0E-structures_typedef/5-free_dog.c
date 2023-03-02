6f79ddd57a4b6c7043497ea3eac152b0afac5ef9
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-03-01 22:49:35 -0800

    who let the dogs out?

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+  * free_dog - function that frees dogs.
+  * @d: dog_type string memalloc
+  * Return: return void if null & || freed
+  */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		return;
+	}
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
