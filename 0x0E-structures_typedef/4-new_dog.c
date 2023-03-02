a91e512323b9143a84eb9789320689711f218b7c
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-03-01 22:32:37 -0800

    memleak?S

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,64 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+  * new_dog - function that creates a new dog.
+  *
+  * @name: name of new_dog for dog_type struct.
+  * @age: age of new_dog for dog_type struct.
+  * @owner: owner name of new_dog of dog_type struct.
+  *
+  * Return: initialized new 'dog_t' object 'new_dog'
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int name_len = 0, owner_len = 0, index;
+	int success = 0;
+
+	dog_t *dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);/*allocation failed*/
+	}
+	/*len & memalloc & index of name string*/
+	while (name[name_len] != '\0')
+	{
+		name_len++;
+	}
+	dog->name = malloc(name_len + 1);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (index = 0; index <= name_len; index++)
+	{
+		dog->name[index] = name[index];
+	}
+	/*len & memalloc & index of owner string*/
+	while (owner[owner_len] != '\0')
+	{
+		owner_len++;
+	}
+	dog->owner = malloc(owner_len + 1);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (index = 0; index <= owner_len; index++)
+	{
+		dog->owner[index] = owner[index];
+	}
+	dog->age = age;
+	success = 1;
+	/*cleanup section*/
+	if (!success)
+	{
+		free(dog->name);
+		free(dog->owner);
+		free(dog);
+		exit(EXIT_FAILURE);
+	}
+	return (dog);
+}
