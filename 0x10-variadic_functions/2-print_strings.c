2bfdc88f2c6d6db2ea4ca0d54cb9f609be33632b
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-03-06 09:44:23 -0800

    fixed typo

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,14 +25,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		string = va_arg(args, char *);
 		if (string == NULL)
 		{
-			printf("nil");
+			printf("(nil)");
 		}
-		else
-		{
 			printf("%s", string);
-		}
 
-		if (index != (n - 1) && separator != NULL)
+		if (index != (n - 1))
 		{
 			printf("%s", separator);
 		}
