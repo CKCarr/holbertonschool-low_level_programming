e9b4cd17ff8d24ee63da226b4315f82ac48e6ebe
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-03-06 09:35:57 -0800

    try again

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 /**
@@ -23,9 +24,10 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op)
 	{
-		if (strcmp(ops[i].op, s) == 0)
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 		i++;
 	}
-	return (NULL);
+	printf("Error\n");
+	exit(99);
 }
