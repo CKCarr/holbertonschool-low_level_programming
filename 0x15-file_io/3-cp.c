6105608edc0ca5f7693067aeb9ad2ffa00d8fd5c
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-03-20 14:06:32 -0700

    ok

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -46,12 +46,12 @@ int main(int argc, char *argv[])
 	}
 	if (close(fd_from) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %i\n", fd_from);
+		dprintf(2, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
 	if (close(fd_to) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %i\n", fd_to);
+		dprintf(2, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 	return (0);
