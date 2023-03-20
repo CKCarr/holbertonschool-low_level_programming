64423aaa981d1d33559f92a201a32d5db86760df
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-03-20 14:03:24 -0700

    typos and more

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#define BUFFER_SIZE 1024
+#define BUFF_SIZE 1024
 /**
 * main - Write a program that copies the content
 *       of a file to another file.
@@ -10,48 +10,48 @@
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, fd_RD, fd_WR;
-	char *buffer[BUFFER_SIZE];
+	char *buffer[BUFF_SIZE];
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd_from = open(argv[1], O_RDONLY, 0);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can's write to %s\n", argv[2]);
+		dprintf(2, "Error: Can's write to %s\n", argv[2]);
 		exit(99);
 	}
-	while ((fd_RD = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	while ((fd_RD = read(fd_from, buffer, BUFF_SIZE)) > 0)
 	{
 		fd_WR = write(fd_to, buffer, fd_RD);
 		if (fd_WR != fd_RD)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 	if (fd_RD == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (close(fd_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		dprintf(2, "Error: Can't close fd %i\n", fd_from);
 		exit(100);
 	}
 	if (close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		dprintf(2, "Error: Can't close fd %i\n", fd_to);
 		exit(100);
 	}
 	return (0);
